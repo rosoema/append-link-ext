f5f998ec2bf880e191a9e465c91c75916f18763d
Ema Roso <roso.emawd@gmail.com>
2022-05-31 12:41:54 +0200

    styling changes

--- src/components/styled/Input.js
+++ src/components/styled/Input.js
@@ -5,11 +5,10 @@ export default styled.input`
     height: 30px;
     width: 94%;
     background: transparent;
-    border: #32E0C4 solid;
+    border: ${prop => prop.editBase ? "#F2FA5A solid" : "#32E0C4 solid"};
     border-width: 0 0 1px 0;
     &:focus {
         outline: none;
-        border: #32E0C4 solid;
         border-width: 0 0 1px 0;
     }
     font-family: "Montserrat";
